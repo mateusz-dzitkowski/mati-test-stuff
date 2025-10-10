b212908cb439fafae8492042d651ad7adadbe262
mateusz.dzitkowski <mateusz.dzitkowski@irdeto.com>
2025-10-10 20:36:35 +0200

    add vector

---
+++ redis/c/test_vector.c
@@ -0,0 +1,113 @@
+#include <assert.h>
+
+#include "vector.c"
+
+void test_vector_is_full() {
+    vector_t* v = vector_new();
+    vector_init(v);
+
+    assert(vector_is_full(v) == false);
+
+    for (int i = 0; i < CAPACITY_STEP; i++) {
+        vector_push_back(v, 0);
+    }
+
+    assert(vector_is_full(v) == true);
+
+    vector_free(v);
+}
+
+void test_vector_push_back() {
+    vector_t* v = vector_new();
+    vector_init(v);
+
+    vector_push_back(v, 123);
+
+    assert(v->size == 1);
+    assert(v->capacity == CAPACITY_STEP);
+    assert(v->elements[0] == 123);
+
+    vector_free(v);
+}
+
+void test_vector_at() {
+    vector_t* v = vector_new();
+    vector_init(v);
+
+    vector_push_back(v, 123);
+    vector_push_back(v, 321);
+
+    assert(vector_at(v, 0) == 123);
+    assert(vector_at(v, 1) == 321);
+
+    vector_free(v);
+}
+
+void test_vector_pop_back() {
+    vector_t* v = vector_new();
+    vector_init(v);
+
+    vector_push_back(v, 123);
+    vector_push_back(v, 321);
+
+    assert(v->size == 2);
+    assert(vector_at(v, 0) == 123);
+    assert(vector_at(v, 1) == 321);
+
+    vector_pop_back(v);
+
+    assert(v->size == 1);
+    assert(vector_at(v, 0) == 123);
+
+    vector_free(v);
+}
+
+void test_vector_erase_1() {
+    vector_t* v = vector_new();
+    vector_init(v);
+
+    vector_push_back(v, 111);
+    vector_push_back(v, 222);
+    vector_push_back(v, 333);
+    vector_push_back(v, 444);
+    vector_push_back(v, 555);
+
+    assert(v->size == 5);
+    assert(vector_at(v, 1) == 222);
+
+    vector_erase(v, 2, 2);
+
+    assert(v->size == 3);
+    assert(vector_at(v, 1) == 222);
+    assert(vector_at(v, 2) == 555);
+
+    vector_free(v);
+}
+
+void test_vector_erase_2() {
+    vector_t* v = vector_new();
+    vector_init(v);
+
+    vector_push_back(v, 111);
+    vector_push_back(v, 222);
+    vector_push_back(v, 333);
+
+    assert(v->size == 3);
+    assert(vector_at(v, 1) == 222);
+
+    vector_erase(v, 1, 2);
+
+    assert(v->size == 1);
+    assert(vector_at(v, 0) == 111);
+
+    vector_free(v);
+}
+
+int main() {
+    test_vector_is_full();
+    test_vector_push_back();
+    test_vector_at();
+    test_vector_pop_back();
+    test_vector_erase_1();
+    test_vector_erase_2();
+}
---
+++ redis/c/vector.c
@@ -0,0 +1,82 @@
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+const int CAPACITY_STEP = 4;
+
+typedef struct vector_t {
+    int* elements;
+    int capacity;
+    int size;
+} vector_t;
+
+vector_t* vector_new() {
+    return malloc(sizeof(vector_t));
+}
+
+void vector_init(vector_t* v) {
+    v->elements = calloc(CAPACITY_STEP, sizeof(int));
+    v->capacity = CAPACITY_STEP;
+    v->size = 0;
+}
+
+bool vector_is_full(const vector_t* v) {
+    return v->size > 0 && v->size % v->capacity == 0;
+}
+
+void vector_free(vector_t* v) {
+    free(v->elements);
+    free(v);
+}
+
+void vector_push_back(vector_t* v, const int elem) {
+    if (vector_is_full(v)) {
+        v->capacity = v->size + CAPACITY_STEP;
+        int *temp = realloc(v->elements, v->capacity * sizeof(int));
+        if (temp == NULL) {
+            vector_free(v);
+            abort();
+        }
+        v->elements = temp;
+    }
+    v->elements[v->size++] = elem;
+}
+
+int vector_at(const vector_t* v, const size_t index) {
+    return v->elements[index];
+}
+
+void vector_pop_back(vector_t* v) {
+    if (v->size == 0) {
+        return;
+    }
+    v->elements[v->size - 1] = 0;
+    v->size--;
+}
+
+void vector_erase(vector_t* v, const int start_index, const uint8_t erase_amount) {
+    for (int i = start_index; i + erase_amount <= v->size; i++) {
+        v->elements[i] = v->elements[i + erase_amount];
+    }
+
+    v->size -= erase_amount;
+}
+
+void vector_erase_one(vector_t* v, const int index) {
+    vector_erase(v, index, 1);
+}
+
+void vector_print_elements(const vector_t* v) {
+    printf("[");
+    for (int i = 0; i < v->size; i++) {
+        if (i != 0) {
+            printf(",");
+        }
+        printf("%d", vector_at(v, i));
+    }
+    printf("]\n");
+}
+
+void vector_print_stats(const vector_t* v) {
+    printf("size: %d, cap: %d\n", v->size, v->capacity);
+}
