e341f210e3ee82aba59f30a983cd5839f0790e12
mateusz.dzitkowski <mateusz.dzitkowski@irdeto.com>
2025-12-02 20:08:28 +0100

    puzzle 2 part 2 wtf

--- ocaml/aoc/bin/main.ml
+++ ocaml/aoc/bin/main.ml
@@ -16,6 +16,7 @@ let () =
     | (1, false) -> Result.Ok Aoc.Ex1.solve_first
     | (1, true) -> Result.Ok Aoc.Ex1.solve_second
     | (2, false) -> Result.Ok Aoc.Ex2.solve_first
+    | (2, true) -> Result.Ok Aoc.Ex2.solve_second
     | (_, _) -> Result.Error "unknown exercise"
     in match result_f with
     | Ok f -> Printf.printf "exercise %d, stage %d - result = %d" !exercise_number stage (f data)
--- ocaml/aoc/lib/ex2.ml
+++ ocaml/aoc/lib/ex2.ml
@@ -9,11 +9,37 @@ let rec range x y =
 
 let id_range_to_id_list r = range r.first r.last
 
+let divisors n =
+    let rec aux i acc =
+        if i > n then List.rev acc
+        else if n mod i = 0 then aux (i + 1) (i :: acc)
+        else aux (i + 1) acc
+    in
+    aux 1 []
+
+let rec any f = function
+    | [] -> false
+    | x :: ls -> f x || any f ls
+
+let is_repeated n s =
+    let substr = String.sub s 0 n in
+    let m = String.length s in
+    m != n
+    && m mod n = 0
+    && String.concat "" (Array.to_list (Array.make (m / n) substr)) = s
+
 let is_invalid s =
-    String.length s mod 2 == 0
-    && String.sub s 0 (String.length s / 2) = String.sub s (String.length s / 2) (String.length s / 2)
+    divisors (String.length s)
+    |> List.map (fun d -> is_repeated d s)
+    |> any (fun x -> x)
 
 let sum_invalid lst = lst
+    |> List.map string_of_int
+    |> List.filter (is_repeated 2)
+    |> List.map int_of_string
+    |> List.fold_left (+) 0
+
+let sum_invalid2 lst = lst
     |> List.map string_of_int
     |> List.filter is_invalid
     |> List.map int_of_string
@@ -36,4 +62,15 @@ let solve_first data = data
     |> List.filter_map (fun x -> x)
     |> List.map id_range_to_id_list
     |> List.map sum_invalid
+    |> List.fold_left (+) 0
+
+let solve_second data = data
+    |> String.split_on_char ','
+    |> List.map (fun s -> s
+        |> String.split_on_char '-'
+        |> list_to_id_range
+    )
+    |> List.filter_map (fun x -> x)
+    |> List.map id_range_to_id_list
+    |> List.map sum_invalid2
     |> List.fold_left (+) 0
